e07088c8dd0b71cc830e12759d2fba2ff90face8
Pablo_Rufat_Munoz <pablo.munoz.ext@talentfour.com.br>
2021-05-26 18:18:44 -0300

    clone cidadao app

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,42 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateAdministrador = /* GraphQL */ `
+  subscription OnCreateAdministrador {
+    onCreateAdministrador {
+      id
+      address
+      nome
+      cpf
+      cfm
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateAdministrador = /* GraphQL */ `
+  subscription OnUpdateAdministrador {
+    onUpdateAdministrador {
+      id
+      address
+      nome
+      cpf
+      cfm
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteAdministrador = /* GraphQL */ `
+  subscription OnDeleteAdministrador {
+    onDeleteAdministrador {
+      id
+      address
+      nome
+      cpf
+      cfm
+      createdAt
+      updatedAt
+    }
+  }
+`;
